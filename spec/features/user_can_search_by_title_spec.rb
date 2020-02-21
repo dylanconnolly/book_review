a731937e6629183b373b58de4f7f66b216e2b18e
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-21 10:44:20 -0700

    Update spec to find css identifiers

--- spec/features/user_can_search_by_title_spec.rb
+++ spec/features/user_can_search_by_title_spec.rb
@@ -16,10 +16,32 @@ RSpec.describe "as a user" do
   end
 
   it "when I search for a book by title I also see reviews from the NYT for that book" do
-    
+    visit root_path
+
+    fill_in :title, with: "the man who saw everything"
+
+    click_button "Find Book"
+
+    expect(current_path).to eq('/search')
+
+    expect(page).to have_css('#reviews')
+    within('#reviews') do
+      expect(first('.review')).to have_css('.summary')
+      expect(first('.review')).to have_css('.date')
+    end
+
+    expect(page).to have_css('.review', count: 2)
   end
 end
 
+
+# I should also see:
+# - This book has 2 reviews from the New York Times
+#   (Note: reviews are the "summary" in the book review response)
+# - Review 1: ""The Man Who Saw Everything,"" which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at "authoritarian old men.""
+# Review Publication Date: 2019-10-09
+# - Review 2: "Deborah Levy's latest novel, "The Man Who Saw Everything," experiments with time travel, history and the endless complications of love."
+# Review Publication Date: 2019-10-15```
 
 
 
