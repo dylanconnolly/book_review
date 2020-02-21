f4e72e1e7e106caf4f874006c57f7c049f613e2e
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-21 11:11:22 -0700

    Remove commented out code from search controller file

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,8 @@
 class SearchController < ApplicationController
 
   def index
-    # library_service = OpenLibraryService.new
-    # book_api_data = library_service.get_book_by_title(params[:title])
-    #
-    # nyt_service = NytBooksService.new
-    # reviews_data = nyt_service.get_reviews_by_title(params[:title])[:results]
-
     render locals: {
       search_results: SearchResultsFacade.new(params[:title])
     }
-    # render locals: {
-    #   book: Book.new(book_api_data),
-    #   search_input: params[:title],
-    #   reviews: reviews_data.map do |data|
-    #               Review.new(data)
-    #             end
-    # }
   end
 end
