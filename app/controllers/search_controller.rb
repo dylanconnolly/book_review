f7be54720ed95c638db077c3a11b8b13fdea5e9c
Dylan Connolly <dylanconnolly145@gmail.com>
2020-02-21 10:44:54 -0700

    Add local for reviews information to index action

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,18 @@
 class SearchController < ApplicationController
 
   def index
-    service = OpenLibraryService.new
-    api_data = service.get_book_by_title(params[:title])
+    library_service = OpenLibraryService.new
+    book_api_data = library_service.get_book_by_title(params[:title])
+
+    nyt_service = NytBooksService.new
+    reviews_data = nyt_service.get_reviews_by_title(params[:title])[:results]
 
     render locals: {
-      book: Book.new(api_data),
-      search_input: params[:title]
+      book: Book.new(book_api_data),
+      search_input: params[:title],
+      reviews: reviews_data.map do |data|
+                  Review.new(data)
+                end
     }
   end
 end
